611f975a8cc212ee37ba7b6ac0a790044eae77d5
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-07-11 15:19:00 +0100

    fix some problems

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -36,28 +36,32 @@ export const addRemoveFriend = async (req, res) => {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
-
-    if (user.friends.includes(friendId)) {
-      user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
-    } else {
-      user.friends.push(friendId);
-      friend.friends.push(id);
-    }
-    await user.save();
-    await friend.save();
-
-    const friends = await Promise.all(
-      user.friends.map((id) => User.findById(id))
-    );
-    const formattedFriends = friends.map(
-      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return { _id, firstName, lastName, occupation, location, picturePath };
+    if (user._id === friend._id) {
+      return res.status(404).json({ message: "Error" });
+    }else{
+      if (user.friends.includes(friendId)) {
+        user.friends = user.friends.filter((id) => id !== friendId);
+        friend.friends = friend.friends.filter((id) => id !== id);
+      } else {
+        user.friends.push(friendId);
+        friend.friends.push(id);
       }
-    );
-
-    res.status(200).json(formattedFriends);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
+      await user.save();
+      await friend.save();
+  
+      const friends = await Promise.all(
+        user.friends.map((id) => User.findById(id))
+      );
+      const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+          return { _id, firstName, lastName, occupation, location, picturePath };
+        }
+      );
+  
+      res.status(200).json(formattedFriends);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+    }
+    
 };
